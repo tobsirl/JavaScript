7ddb4e09311be801a48713bab27fbf42a1b7086b
Paul Tobin <tobster111@gmail.com>
2020-10-21 20:01:31 +0100

    Finish assignments

--- javascript-course/01-Fundamentals-Part-1/assignment.js
+++ javascript-course/01-Fundamentals-Part-1/assignment.js
@@ -1,7 +1,29 @@
+// values and variables
 const country = 'Ireland';
 const continent = 'Europe';
-const population = 4904000;
+let population = 4904000;
+
+// data types
+const isIsland = true;
+let language;
+
+// let var and const
+language = 'English';
+
+// basic operators
+const split = population / 2;
+const popPlusOne = population + 1;
+const biggestPop = population > 6000000;
+const avgPop = population >= 33000000;
+const description = country + ' is in ' + continent + ', and its ' + population;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+console.log(isIsland);
+console.log(language);
+console.log(split);
+console.log(popPlusOne);
+console.log(biggestPop);
+console.log(avgPop);
+console.log(description);
