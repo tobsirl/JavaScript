42e44b0c7d6d8988f9d3209bafaa72a5e5ed2905
Paul Tobin <tobster111@gmail.com>
2020-10-21 21:11:27 +0100

    Coding Challenge 1 complete

---
+++ javascript-course/01-Fundamentals-Part-1/script.js
@@ -0,0 +1,18 @@
+// const markMass = 78;
+// const markHeight = 1.69;
+
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+const markMass = 95;
+const markHeight = 1.88;
+
+const johnMass = 85;
+const johnHeight = 1.76;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI, johnBMI, markHigherBMI);
