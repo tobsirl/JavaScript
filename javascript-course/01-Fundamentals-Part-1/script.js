1853e35a009d2ac1311a2c6a2c160f8649b2e451
Paul Tobin <tobster111@gmail.com>
2020-11-04 20:53:30 +0000

    Coding Challenge 2

--- javascript-course/01-Fundamentals-Part-1/script.js
+++ javascript-course/01-Fundamentals-Part-1/script.js
@@ -1,18 +1,32 @@
-// const markMass = 78;
-// const markHeight = 1.69;
+const markMass = 78;
+const markHeight = 1.69;
 
-// const johnMass = 92;
-// const johnHeight = 1.95;
+const johnMass = 92;
+const johnHeight = 1.95;
 
-const markMass = 95;
-const markHeight = 1.88;
+// const markMass = 95;
+// const markHeight = 1.88;
 
-const johnMass = 85;
-const johnHeight = 1.76;
+// const johnMass = 85;
+// const johnHeight = 1.76;
 
 const markBMI = markMass / markHeight ** 2;
 const johnBMI = johnMass / johnHeight ** 2;
 
 const markHigherBMI = markBMI > johnBMI;
 
-console.log(markBMI, johnBMI, markHigherBMI);
+if (markHigherBMI) {
+  console.log(
+    `Mark's BMI (${markBMI.toFixed(
+      2
+    )}) is higher then John's (${johnBMI.toFixed(2)})`
+  );
+} else {
+  console.log(
+    `John's BMI (${johnBMI.toFixed(
+      2
+    )}) is higher then Mark's (${markBMI.toFixed(2)})`
+  );
+}
+
+console.log();
