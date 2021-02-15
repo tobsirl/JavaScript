5fd02bf76d2ca8cdc13e982a2a480403030afc25
Paul Tobin <tobster111@gmail.com>
2021-02-15 19:32:00 +0000

    Displaying the notes

--- Google-Keep-Clone/app.js
+++ Google-Keep-Clone/app.js
@@ -7,6 +7,7 @@ class App {
     this.$noteTitle = document.querySelector('#note-title');
     this.$noteText = document.querySelector('#note-text');
     this.$formButtons = document.querySelector('#form-buttons');
+    this.$notes = document.querySelector('#notes');
 
     this.addEventListeners();
   }
@@ -68,6 +69,27 @@ class App {
   displayNotes() {
     const hasNotes = this.notes.length > 0;
     this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
+
+    this.$notes.innerHTML = this.notes
+      .map(
+        (note) => `
+    <div style="background: ${note.color}" class="note">
+      <div class="${note.title && 'note-title'}">
+        ${note.title}
+      </div>
+      <div class="note-text">
+        ${note.text}
+      </div>
+      <div class="toolbar-container">
+      <div class="toolbar">
+      <img class="toolbar-color" src="https://icon.now.sh/palette">
+      <img class="toolbar-delete" src="https://icon.now.sh/delete">
+    </div>
+          </div>
+    </div>
+    `
+      )
+      .join('');
   }
 }
 
