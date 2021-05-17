94988addc5c617293a9beab76cc5e3d223b93d8d
Paul Tobin <tobster111@gmail.com>
2021-05-17 20:34:32 +0100

    Replace an item at an index

--- transform-data/index.js
+++ transform-data/index.js
@@ -1,2 +1,11 @@
-// Delete an item from an array
-const newPlayers = players.filter((player) => player.id !== ID_TO_DELETE)
+// 1: Delete an item from an array
+const newPlayers = players.filter((player) => player.id !== ID_TO_DELETE);
+
+// 2: Replace an item at an array index
+players[indexToReplace] = newPlayer;
+
+const newPlayers = [
+  ...players.slice(0, INDEX_TO_REPLACE),
+  newPlayer,
+  ...players.slice(INDEX_TO_REPLACE + 1),
+];
