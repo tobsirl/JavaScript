1178d545cc0ceea670cbb629057b2101443bb02e
Paul Tobin <tobster111@gmail.com>
2021-05-17 20:44:10 +0100

    Array.from to fill out arrays

--- transform-data/index.js
+++ transform-data/index.js
@@ -1,7 +1,7 @@
-// 1: Delete an item from an array
+// 1. Delete an item from an array
 const newPlayers = players.filter((player) => player.id !== ID_TO_DELETE);
 
-// 2: Replace an item at an array index
+// 2. Replace an item at an array index
 players[indexToReplace] = newPlayer;
 
 const newPlayers = [
@@ -9,3 +9,16 @@ const newPlayers = [
   newPlayer,
   ...players.slice(INDEX_TO_REPLACE + 1),
 ];
+
+// 3. Create an empty array of X items
+Array.from({ length: 5 }, (_, index) => index);
+// => [0, 1, 2, 3, 4]
+
+Array.from({ length: 5 }, () => '');
+// => ['', '', '', '', '']
+
+Array.from({ length: 5 }, (_, index) => index * 2);
+// => [0, 2, 4, 6, 8]
+
+Array.from({ length: 5 });
+// => [undefined, undefined, undefined, undefined, undefined]
