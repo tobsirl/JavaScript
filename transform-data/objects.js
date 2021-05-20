2fad33129b859e5047af4e0ea1d3bb0d484811cb
Paul Tobin <tobster111@gmail.com>
2021-05-20 18:44:41 +0100

    Example using Object.values practical use

--- transform-data/objects.js
+++ transform-data/objects.js
@@ -11,3 +11,15 @@ const ageExists = Object.keys(user).includes('age'); //?
 // Using Object.values()
 Object.values(user); //?
 
+const monthlyExpenses = {
+  food: 400,
+  rent: 1700,
+  insurance: 550,
+  internet: 49,
+  phone: 95,
+};
+
+const monthlyTotal = Object.values(monthlyExpenses).reduce(
+  (total, expence) => total + expence,
+  0
+); //?
