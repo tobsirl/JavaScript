aa1e271ecd0ab06f1ad4996e24c6dce95c4cfa44
Paul Tobin <tobster111@gmail.com>
2021-07-03 11:53:56 +0100

    Added items

--- fastify-api/server.js
+++ fastify-api/server.js
@@ -1,8 +1,10 @@
 const fastify = require('fastify')({logger: true})
 const PORT = 5000
 
+const items = require('./items')
+
 fastify.get('/items', (req, reply) => {
-  reply.send({message: "Hello"})
+  reply.send(items)
 })
 
 const start = async() => {
