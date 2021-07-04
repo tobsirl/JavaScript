6e0d5026af04611826c51a50cac5c81d2adf2d54
Paul Tobin <tobster111@gmail.com>
2021-07-04 11:21:27 +0100

    Using getItemOpts

--- fastify-api/routes/items.js
+++ fastify-api/routes/items.js
@@ -1,7 +1,25 @@
 const items = require('../items');
 
+// Options for get all items
+const getItemsOpts = {
+  scheme: {
+    response: {
+      200: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            id: { type: 'string' },
+            name: { type: 'string' },
+          },
+        },
+      },
+    },
+  },
+};
+
 function itemRoutes(fastify, options, done) {
-  fastify.get('/items', (req, reply) => {
+  fastify.get('/items', getItemsOpts, (req, reply) => {
     reply.send(items);
   });
 
