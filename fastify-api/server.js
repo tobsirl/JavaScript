7d5254c477654b576989d42cfbcfca5d78651939
Paul Tobin <tobster111@gmail.com>
2021-07-04 12:18:23 +0100

    Added fastify swagger

--- fastify-api/server.js
+++ fastify-api/server.js
@@ -1,5 +1,13 @@
 const fastify = require('fastify')({ logger: true });
 
+fastify.register(require('fastify-swagger'), {
+  exposeRoute: true,
+  routePrefix: '/docs',
+  swagger: {
+    info: { title: 'fastify-api' },
+  },
+});
+
 fastify.register(require('./routes/items'));
 
 const PORT = 5000;
