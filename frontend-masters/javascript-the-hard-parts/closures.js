1bb10f6c7c5c47b632e3323439a3f9ee75a14b39
Paul Tobin <tobster111@gmail.com>
2021-11-13 21:27:39 +0000

    Closures challenge 3

--- frontend-masters/javascript-the-hard-parts/closures.js
+++ frontend-masters/javascript-the-hard-parts/closures.js
@@ -54,17 +54,17 @@ function addByX(x) {
 
 // /*** Uncomment these to check your work! ***/
 const addByTwo = addByX(2);
-console.log(addByTwo(1)); // => should return 3
-console.log(addByTwo(2)); // => should return 4
+addByTwo(1); // => should return 3
+addByTwo(2); // => should return 4
 addByTwo(3); // => should return 5
 
-// const addByThree = addByX(3);
-// addByThree(1); // => should return 4
-// addByThree(2); // => should return 5
+const addByThree = addByX(3);
+addByThree(1); // => should return 4
+addByThree(2); // => should return 5
 
-// const addByFour = addByX(4);
-// addByFour(4); // => should return 8
-// addByFour(5); // => should return 9
+const addByFour = addByX(4);
+addByFour(4); // => should return 8
+addByFour(5); // => should return 9
 
 // CHALLENGE 4
 function once(func) {}
