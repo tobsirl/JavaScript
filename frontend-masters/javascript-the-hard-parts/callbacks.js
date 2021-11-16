f1d2ec40e31b1bb2e9c7f6008a0c5d5b632893bc
Paul Tobin <tobster111@gmail.com>
2021-11-16 19:40:28 +0000

    Intersetion

--- frontend-masters/javascript-the-hard-parts/callbacks.js
+++ frontend-masters/javascript-the-hard-parts/callbacks.js
@@ -74,9 +74,12 @@ function reduce(array, callback, initialValue) {
 console.log(reduce(nums, add, 0));
 
 // Challenge 7
-function intersection(arrays) {}
+// Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs. BONUS: Use reduce!
+function intersection(arrays) {
 
-// console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
+}
+
+console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
 // should log: [5, 15]
 
 // Challenge 8
