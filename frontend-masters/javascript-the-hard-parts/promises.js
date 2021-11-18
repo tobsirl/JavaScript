68f424e1a34f4ed461fc3447367c199f1de7b531
Paul Tobin <tobster111@gmail.com>
2021-11-18 18:40:03 +0000

    Challenge 3

--- frontend-masters/javascript-the-hard-parts/promises.js
+++ frontend-masters/javascript-the-hard-parts/promises.js
@@ -27,6 +27,10 @@ var promise = new Promise(function (resolve, reject) {
 promise.then((data) => console.log(data));
 
 // Challenge 3
+/*
+ * Create another promise. Now have it reject with a value of "Rejected!" without using setTimeout. Print the contents of the promise after it has been rejected by
+ * passing console.log to .catch
+ */
 
 promise = new Promise(function (resolve, reject) {
   // ADD CODE HERE
