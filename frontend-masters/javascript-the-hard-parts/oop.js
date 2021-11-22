e7eccc97c010abfda621e4194c320971c226e378
Paul Tobin <tobster111@gmail.com>
2021-11-22 18:54:58 +0000

    Solution

--- frontend-masters/javascript-the-hard-parts/oop.js
+++ frontend-masters/javascript-the-hard-parts/oop.js
@@ -2,24 +2,27 @@
                   WORKING WITH OBJECT LITERALS
 ****************************************************************/
 
-/*** CHALLENGE 1 ***/
+/*** CHALLENGE 1 
+Create a function that has two parameters (name and age) and returns an object. Let's call this function makePerson. This function will:
+Create an empty object
+Add a name property to the newly created object with its value being the 'name' argument passed into the function
+Add an age property to the newly created object with its value being the 'age' argument passed into the function
+Return the object
+***/
 
 function makePerson(name, age) {
-	// add code here
-
-
+  // add code here
+  let person = {};
+  person.name = name;
+  person.age = age;
+  return person;
 }
 
 const vicky = makePerson('Vicky', 24);
 
-
 // /********* Uncomment these lines to test your work! *********/
-// console.log(vicky.name); // -> Logs 'Vicky'
-// console.log(vicky.age); // -> Logs 24
-
-
-
-
+console.log(vicky.name); // -> Logs 'Vicky'
+console.log(vicky.age); // -> Logs 24
 
 /****************************************************************
                        USING OBJECT.CREATE
@@ -28,43 +31,30 @@ const vicky = makePerson('Vicky', 24);
 /*** CHALLENGE 2 ***/
 
 const personStore = {
-	// add code here
-
-
+  // add code here
 };
 
 // /********* Uncomment this line to test your work! *********/
 // personStore.greet(); // -> Logs 'hello'
 
-
-
 /*** CHALLENGE 3 ***/
 
 function personFromPersonStore(name, age) {
-	// add code here
-
-
+  // add code here
 }
 
 const sandra = personFromPersonStore('Sandra', 26);
-
 
 // /********* Uncomment these lines to test your work! *********/
 // console.log(sandra.name); // -> Logs 'Sandra'
 // console.log(sandra.age); //-> Logs 26
 // sandra.greet(); //-> Logs 'hello'
 
-
-
 /*** CHALLENGE 4 ***/
 
 // add code here
 
 // sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
-
-
-
-
 
 /****************************************************************
                     USING THE 'NEW' KEYWORD
@@ -73,42 +63,30 @@ const sandra = personFromPersonStore('Sandra', 26);
 /*** CHALLENGE 5 ***/
 
 function PersonConstructor() {
-	// add code here
-
-
+  // add code here
 }
 
-
 // /********* Uncomment this line to test your work! *********/
-const simon = new PersonConstructor;
+const simon = new PersonConstructor();
 // simon.greet(); // -> Logs 'hello'
-
-
 
 /*** CHALLENGE 6 ***/
 
 function personFromConstructor(name, age) {
-	// add code here
-
-
+  // add code here
 }
 
 const mike = personFromConstructor('Mike', 30);
-
 
 // /********* Uncomment these lines to test your work! *********/
 // console.log(mike.name); // -> Logs 'Mike'
 // console.log(mike.age); //-> Logs 30
 // mike.greet(); //-> Logs 'hello'
 
-
-
 /*** CHALLENGE 7 ***/
 // add code here
 
-
 // mike.introduce(); // -> Logs 'Hi, my name is Mike'
-
 
 /****************************************************************
                         USING ES6 CLASSES
@@ -117,48 +95,39 @@ const mike = personFromConstructor('Mike', 30);
 /*** CHALLENGE 8 ***/
 
 class PersonClass {
-	constructor() {
+  constructor() {
     // add code here
+  }
 
-
-	}
-
-	// add code here
-
+  // add code here
 }
 
-
 // /********* Uncomment this line to test your work! *********/
-const george = new PersonClass;
+const george = new PersonClass();
 // george.greet(); // -> Logs 'hello'
-
-
 
 /*** CHALLENGE 9 ***/
 
 // add code here
-
 
 // /********* Uncomment these lines to test your work! *********/
 // const thai = new DeveloperClass('Thai', 32);
 // console.log(thai.name); // -> Logs 'Thai'
 // thai.introduce(); //-> Logs 'Hello World, my name is Thai'
 
-
-
 /****************************************************************
                       EXTENSION: SUBCLASSING
 ****************************************************************/
 
 const userFunctionStore = {
-  sayType: function() {
-    console.log("I am a " + this.type);
-  }
-}
+  sayType: function () {
+    console.log('I am a ' + this.type);
+  },
+};
 
 function userFactory(name, score) {
   let user = Object.create(userFunctionStore);
-  user.type = "User";
+  user.type = 'User';
   user.name = name;
   user.score = score;
   return user;
@@ -168,7 +137,7 @@ function userFactory(name, score) {
 
 const adminFunctionStore = {
   // add code here
-}
+};
 
 /*** CHALLENGE 11, 12, 13 ***/
 
@@ -179,12 +148,11 @@ function adminFactory(name, score) {
 /*** CHALLENGE 14 ***/
 /* Put code here for a method called sharePublicMessage*/
 
-const adminFromFactory = adminFactory("Eva", 5);
+const adminFromFactory = adminFactory('Eva', 5);
 
 // /********* Uncomment these lines to test your work! *********/
 // adminFromFactory.sayType() // -> Logs "I am a Admin"
 // adminFromFactory.sharePublicMessage() // -> Logs "Welcome users!"
-
 
 /****************************************************************
 EXTENSION: MIXINS
@@ -201,8 +169,10 @@ class Dog {
 
 const robotMixin = {
   skin: 'metal',
-  speak: function() { console.log(`I have ${this.legs} legs and am made of ${this.skin}`) },
-}
+  speak: function () {
+    console.log(`I have ${this.legs} legs and am made of ${this.skin}`);
+  },
+};
 
 let robotFido = new Dog();
 
@@ -210,4 +180,3 @@ let robotFido = new Dog();
 
 // /********* Uncomment to test your work! *********/
 // robotFido.speak() // -> Logs "I am made of metal"
-
