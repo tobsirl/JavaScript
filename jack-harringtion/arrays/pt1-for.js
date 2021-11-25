6765428217db9c4bc9124c81e0db3c732057c8ab
Paul Tobin <tobster111@gmail.com>
2021-11-25 22:23:19 +0000

    Bad example

--- jack-harringtion/arrays/pt1-for.js
+++ jack-harringtion/arrays/pt1-for.js
@@ -3,3 +3,7 @@ const arrayOfNumbers = [];
 for (let value = 10; value <= 50; value += 10) {
   arrayOfNumbers.push(value);
 }
+
+for (let index = 0; index < arrayOfNumbers.length; index++) {
+  arrayOfNumbers[index];
+}
