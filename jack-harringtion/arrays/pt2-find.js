6a6b06d7863d4dfdc750506fbcd5f78313e0e297
Paul Tobin <tobster111@gmail.com>
2021-11-26 20:39:32 +0000

    Using find

---
+++ jack-harringtion/arrays/pt2-find.js
@@ -0,0 +1,10 @@
+const names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
+
+console.log(names.find((name) => name === 'Alice'));
+
+const people = [{ name: 'John' }, { name: 'Jane' }];
+
+const person = people.find((person) => person.name === 'John');
+person;
+person.name = 'Jimmy';
+person
