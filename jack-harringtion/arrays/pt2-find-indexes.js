2db1efa48f4af1b5de6063da0c7c3091ea548111
Paul Tobin <tobster111@gmail.com>
2021-11-26 20:26:08 +0000

    lastIndexOf

--- jack-harringtion/arrays/pt2-find-indexes.js
+++ jack-harringtion/arrays/pt2-find-indexes.js
@@ -1,3 +1,7 @@
 const names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
 
 console.log(names.indexOf('Alice'));
+console.log(names.indexOf('Alice', 1));
+console.log(names.indexOf('Sally'));
+
+console.log(names.lastIndexOf('Alice'));
