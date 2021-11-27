5d5d58bbfbdc070f6bf9a5d659ab1cd439f848ab
Paul Tobin <tobster111@gmail.com>
2021-11-27 11:20:47 +0000

    under 50

--- jack-harringtion/arrays/pt3-filter-numbers.js
+++ jack-harringtion/arrays/pt3-filter-numbers.js
@@ -5,3 +5,8 @@ allPositive;
 
 const allNegatives = numbers.filter((num) => num < 0);
 allNegatives;
+
+const positiveUnder50 = numbers
+  .filter((num) => num > 0)
+  .filter((num) => num < 50);
+positiveUnder50;
