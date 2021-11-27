708a3b8cf00d51a85031bf1a67f75910fc2d249b
Paul Tobin <tobster111@gmail.com>
2021-11-27 11:51:52 +0000

    Working with references

--- jack-harringtion/arrays/pt3-filter-objects.js
+++ jack-harringtion/arrays/pt3-filter-objects.js
@@ -1,10 +1,12 @@
 const people = [{ name: 'John' }, { name: 'Ann' }];
 
-const nameStartingWithJ = people.filter((person) =>
-  person.name.startsWith('J')
-);
-nameStartingWithJ;
+// const nameStartingWithJ = people.filter((person) =>
+//   person.name.startsWith('J')
+// );
 
 // using destructing
 const nameStartingWithJ = people.filter(({ name }) => name.startsWith('J'));
 nameStartingWithJ;
+nameStartingWithJ[0].name = 'Jack';
+nameStartingWithJ;
+people;
