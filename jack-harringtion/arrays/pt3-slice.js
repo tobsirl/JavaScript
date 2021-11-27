cfd0dbe4f5fc29dd3dac7e8b3faf6d4911e6eaee
Paul Tobin <tobster111@gmail.com>
2021-11-27 12:04:50 +0000

    Slice examples

--- jack-harringtion/arrays/pt3-slice.js
+++ jack-harringtion/arrays/pt3-slice.js
@@ -4,3 +4,8 @@ const middleThree = numbers.filter((_, i, arr) => i > 0 && i < arr.length - 1);
 middleThree;
 
 const easierMiddleThree = numbers.slice(1, 4);
+
+numbers.slice();
+numbers.slice(1);
+numbers.slice(-1);
+numbers.slice(-2);
