5fe4acfbf5706512df42889ed6f948e13ad6644e
Paul Tobin <tobster111@gmail.com>
2021-11-27 11:17:57 +0000

    allNegative

--- jack-harringtion/arrays/pt3-filter-numbers.js
+++ jack-harringtion/arrays/pt3-filter-numbers.js
@@ -1,4 +1,7 @@
 const numbers = [10, -20, 30, -40, 50];
 
 const allPositive = numbers.filter((num) => num > 0);
-allPositive
+allPositive;
+
+const allNegatives = numbers.filter((num) => num < 0);
+allNegatives;
