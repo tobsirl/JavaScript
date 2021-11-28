971d159db626eeda36771354fe44ab2897cc5505
Paul Tobin <tobster111@gmail.com>
2021-11-28 14:07:49 +0000

    Returning an object

--- jack-harringtion/arrays/pt4-map.js
+++ jack-harringtion/arrays/pt4-map.js
@@ -2,3 +2,9 @@ const numbers = [10, 20, 30, 40, 50];
 
 const numbersTimes10 = numbers.map((num) => num * 10);
 numbersTimes10;
+
+const numbersTimes10Obj = numbers.map((num) => ({
+  value: num * 10,
+}));
+
+numbersTimes10Obj;
