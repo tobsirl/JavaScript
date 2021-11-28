deb2710a1e711b910cab4564e52359a6c233033a
Paul Tobin <tobster111@gmail.com>
2021-11-28 14:18:04 +0000

    Using a return

--- jack-harringtion/arrays/pt4-map.js
+++ jack-harringtion/arrays/pt4-map.js
@@ -3,8 +3,12 @@ const numbers = [10, 20, 30, 40, 50];
 const numbersTimes10 = numbers.map((num) => num * 10);
 numbersTimes10;
 
-const numbersTimes10Obj = numbers.map((num) => ({
-  value: num * 10,
-}));
+const numbersTimes10Obj = numbers.map((num) => {
+  return {
+    value: num * 10,
+  };
+});
 
 numbersTimes10Obj;
+
+const numbersWithNegatives = [-10, 20, 30, -40, -50];
