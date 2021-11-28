e23c6862d1981ac734bc1953c197a70eac99b6a0
Paul Tobin <tobster111@gmail.com>
2021-11-28 14:53:37 +0000

    Example of flatMap

--- jack-harringtion/arrays/pt4-flat.js
+++ jack-harringtion/arrays/pt4-flat.js
@@ -10,3 +10,8 @@ const deepNumbers = [[[[10, 20, 30]]], [[[40, 50, 60]]], [[[70, 80, 90]]]];
 deepNumbers.flat();
 deepNumbers.flat(2);
 deepNumbers.flat(Infinity);
+
+const values = [10, 20, 30, 40, 50];
+values.map((value, index) => [value, index]);
+values.flatMap((value, index) => [value, index]);
+
