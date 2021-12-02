0fabd111b3fb4fa435f70f959a8553cde7a34d24
Paul Tobin <tobster111@gmail.com>
2021-12-02 19:58:42 +0000

    Reduce slice

--- jack-harringtion/arrays/pt5-mother.js
+++ jack-harringtion/arrays/pt5-mother.js
@@ -1,5 +1,14 @@
 const numbers = [1, 2, 3, 4, 5];
 
 // Includes
-numbers.reduce((includes, value) => (includes ? includes : value === 3), false); 
-numbers.reduce((includes, value) => (includes ? includes : value === 10), false); 
+numbers.reduce((includes, value) => (includes ? includes : value === 3), false);
+numbers.reduce(
+  (includes, value) => (includes ? includes : value === 10),
+  false
+);
+
+// slice
+numbers.reduce(
+  (arr, value, index) => (index > 0 && index < 4 ? [...arr, value] : arr),
+  []
+);
