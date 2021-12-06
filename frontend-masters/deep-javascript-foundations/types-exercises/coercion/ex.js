789751de72213146c3619a0e346550da740d94db
Paul Tobin <tobster111@gmail.com>
2021-12-06 21:18:53 +0000

    hoursAttended complete

--- frontend-masters/deep-javascript-foundations/types-exercises/coercion/ex.js
+++ frontend-masters/deep-javascript-foundations/types-exercises/coercion/ex.js
@@ -11,9 +11,23 @@ function hoursAttended(attended, length) {
   if (typeof attended === 'string' && attended.trim() != '') {
     attended = Number(attended);
   }
+
   if (typeof length === 'string' && length.trim() != '') {
     length = Number(length);
   }
+
+  if (
+    typeof attended == 'number' &&
+    typeof length == 'number' &&
+    attended >= 0 &&
+    length >= 0 &&
+    Number.isInteger(attended) &&
+    Number.isInteger(length) &&
+    attended <= length
+  ) {
+    return true;
+  }
+  return false;
 }
 
 // tests:
