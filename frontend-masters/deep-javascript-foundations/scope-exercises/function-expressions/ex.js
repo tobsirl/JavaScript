367b2f199b84431b1f98d8dc626dcd7e81589bb9
Paul Tobin <tobster111@gmail.com>
2021-12-18 16:54:59 +0000

    Setup

--- frontend-masters/deep-javascript-foundations/scope-exercises/function-expressions/ex.js
+++ frontend-masters/deep-javascript-foundations/scope-exercises/function-expressions/ex.js
@@ -1,37 +1,36 @@
 function printRecords(recordIds) {
-	// TODO
+  // TODO
 }
 
 function paidStudentsToEnroll() {
-	// TODO
+  // TODO
 }
 
 function remindUnpaid(recordIds) {
-	// TODO
+  // TODO
 }
-
 
 // ********************************
 
-var currentEnrollment = [ 410, 105, 664, 375 ];
+const currentEnrollment = [410, 105, 664, 375];
 
-var studentRecords = [
-	{ id: 313, name: "Frank", paid: true, },
-	{ id: 410, name: "Suzy", paid: true, },
-	{ id: 709, name: "Brian", paid: false, },
-	{ id: 105, name: "Henry", paid: false, },
-	{ id: 502, name: "Mary", paid: true, },
-	{ id: 664, name: "Bob", paid: false, },
-	{ id: 250, name: "Peter", paid: true, },
-	{ id: 375, name: "Sarah", paid: true, },
-	{ id: 867, name: "Greg", paid: false, },
+const studentRecords = [
+  { id: 313, name: 'Frank', paid: true },
+  { id: 410, name: 'Suzy', paid: true },
+  { id: 709, name: 'Brian', paid: false },
+  { id: 105, name: 'Henry', paid: false },
+  { id: 502, name: 'Mary', paid: true },
+  { id: 664, name: 'Bob', paid: false },
+  { id: 250, name: 'Peter', paid: true },
+  { id: 375, name: 'Sarah', paid: true },
+  { id: 867, name: 'Greg', paid: false },
 ];
 
 printRecords(currentEnrollment);
-console.log("----");
+console.log('----');
 currentEnrollment = paidStudentsToEnroll();
 printRecords(currentEnrollment);
-console.log("----");
+console.log('----');
 remindUnpaid(currentEnrollment);
 
 /*
