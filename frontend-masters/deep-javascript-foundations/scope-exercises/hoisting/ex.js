4d989ad4e791cbc1fdc8d839ad6d9fd52dfa7291
Paul Tobin <tobster111@gmail.com>
2021-12-23 15:49:48 +0000

    functions placed at the bottom due to hoisting

--- frontend-masters/deep-javascript-foundations/scope-exercises/hoisting/ex.js
+++ frontend-masters/deep-javascript-foundations/scope-exercises/hoisting/ex.js
@@ -1,3 +1,28 @@
+// ********************************
+
+var currentEnrollment = [410, 105, 664, 375];
+
+var studentRecords = [
+  { id: 313, name: 'Frank', paid: true },
+  { id: 410, name: 'Suzy', paid: true },
+  { id: 709, name: 'Brian', paid: false },
+  { id: 105, name: 'Henry', paid: false },
+  { id: 502, name: 'Mary', paid: true },
+  { id: 664, name: 'Bob', paid: false },
+  { id: 250, name: 'Peter', paid: true },
+  { id: 375, name: 'Sarah', paid: true },
+  { id: 867, name: 'Greg', paid: false },
+];
+
+printRecords(currentEnrollment);
+console.log('----');
+currentEnrollment = paidStudentsToEnroll();
+printRecords(currentEnrollment);
+console.log('----');
+remindUnpaid(currentEnrollment);
+
+
+// **********************************************
 function getStudentFromId(studentId) {
   return studentRecords.find(matchId);
 
@@ -52,29 +77,6 @@ function notYetPaid(studentId) {
   var record = getStudentFromId(studentId);
   return !record.paid;
 }
-
-// ********************************
-
-var currentEnrollment = [410, 105, 664, 375];
-
-var studentRecords = [
-  { id: 313, name: 'Frank', paid: true },
-  { id: 410, name: 'Suzy', paid: true },
-  { id: 709, name: 'Brian', paid: false },
-  { id: 105, name: 'Henry', paid: false },
-  { id: 502, name: 'Mary', paid: true },
-  { id: 664, name: 'Bob', paid: false },
-  { id: 250, name: 'Peter', paid: true },
-  { id: 375, name: 'Sarah', paid: true },
-  { id: 867, name: 'Greg', paid: false },
-];
-
-printRecords(currentEnrollment);
-console.log('----');
-currentEnrollment = paidStudentsToEnroll();
-printRecords(currentEnrollment);
-console.log('----');
-remindUnpaid(currentEnrollment);
 
 /*
 	Bob (664): Not Paid
