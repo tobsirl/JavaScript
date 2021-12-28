64db2f210cae4f91e4ca2c64b4e39a56f445bd32
Paul Tobin <tobster111@gmail.com>
2021-12-28 14:09:09 +0000

    Formatting

--- frontend-masters/javascript-recent-parts/template-strings/ex.js
+++ frontend-masters/javascript-recent-parts/template-strings/ex.js
@@ -1,10 +1,10 @@
-function upper(strings,...values) {}
+function upper(strings, ...values) {}
 
-var name = "kyle",
-	twitter = "getify",
-	topic = "JS Recent Parts";
+var name = 'kyle',
+  twitter = 'getify',
+  topic = 'JS Recent Parts';
 
 console.log(
-	`Hello ____ (@____), welcome to ____!` ===
-	"Hello KYLE (@GETIFY), welcome to JS RECENT PARTS!"
+  `Hello ____ (@____), welcome to ____!` ===
+    'Hello KYLE (@GETIFY), welcome to JS RECENT PARTS!'
 );
