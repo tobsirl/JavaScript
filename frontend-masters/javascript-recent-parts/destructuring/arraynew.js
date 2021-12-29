5489afda27bce0bc342b09ed93e3cbe7c80bdb2f
Paul Tobin <tobster111@gmail.com>
2021-12-29 11:04:27 +0000

    Adding default arrays

--- frontend-masters/javascript-recent-parts/destructuring/arraynew.js
+++ frontend-masters/javascript-recent-parts/destructuring/arraynew.js
@@ -2,4 +2,4 @@ function data() {
   return [1, 2, 3];
 }
 
-const [first, second, third, fourth] = data();
+const [first, second, third, fourth] = data() || [];
--- frontend-masters/javascript-recent-parts/destructuring/arrayold.js
+++ frontend-masters/javascript-recent-parts/destructuring/arrayold.js
@@ -2,7 +2,7 @@ function data() {
   return [1, 2, 3];
 }
 
-const tmp = data();
+const tmp = data() || [];
 
 const first = tmp[0];
 const second = tmp[1];
