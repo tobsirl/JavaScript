b537ac7d2201acdbbc3af601621be7a0c9811f6f
Paul Tobin <tobster111@gmail.com>
2022-01-02 12:47:54 +0000

    Iterators and Generators

--- frontend-masters/javascript-recent-parts/iterators-generators/ex.js
+++ frontend-masters/javascript-recent-parts/iterators-generators/ex.js
@@ -1,14 +1,14 @@
 var numbers = {
-	// ..
+  // ..
 };
 
 // should print 0..100 by 1s
 for (let num of numbers) {
-	console.log(num);
+  console.log(num);
 }
 
 // should print 6..30 by 4s
-console.log("My lucky numbers are: ____");
+console.log('My lucky numbers are: ____');
 
 // Hint:
 //     [...numbers[Symbol.iterator]( ?? )]
