b18dd12a28899e2952aa3898985eaa33992db900
Paul Tobin <tobster111@gmail.com>
2022-01-07 18:26:44 +0000

    Challenge 2

--- frontend-masters/javascript-the-new-hard-parts/promises.js
+++ frontend-masters/javascript-the-new-hard-parts/promises.js
@@ -11,3 +11,14 @@ function sayHello() {
 
 // Uncomment the line below when ready
 sayHello(); // should log "Hello" after 1000ms
+
+/* 
+Challenge 2
+Create a promise. Have it resolve with a value of 'Resolved!' in resolve after a delay of 1000ms, using setTimeout. Print the contents of the promise after it has been resolved by passing console.log to .then
+*/
+var promise = new Promise(function (resolve, reject) {
+  
+});
+
+// Should print out "Resolved!"
+// ADD CODE HERE
