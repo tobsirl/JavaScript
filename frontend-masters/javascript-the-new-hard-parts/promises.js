6187d0ace2a1c5db11aec84d7a3cf10c89830550
Paul Tobin <tobster111@gmail.com>
2022-01-07 18:24:25 +0000

    First challenge complete

--- frontend-masters/javascript-the-new-hard-parts/promises.js
+++ frontend-masters/javascript-the-new-hard-parts/promises.js
@@ -3,7 +3,11 @@ Challenge 1
 Let's start by reviewing asynchronous functions! Using setTimeout, print the string 'Hello!' after 1000ms.
 */
 
-function sayHello() {}
+function sayHello() {
+  setTimeout(() => {
+    console.log('Hello!');
+  }, 1000);
+}
 
 // Uncomment the line below when ready
 sayHello(); // should log "Hello" after 1000ms
