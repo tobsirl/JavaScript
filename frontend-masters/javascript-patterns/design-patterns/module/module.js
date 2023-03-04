e5451cd31aadbb9b61bf35101cde97d872c8fdac
Paul Tobin <tobster111@gmail.com>
2023-03-04 10:08:16 +0000

    Javascript Patterns

--- frontend-masters/javascript-patterns/design-patterns/module/module.js
+++
@@ -1,4 +0,0 @@
-// Overview: Module pattern
-// ES2015 introduced built-in JavaScript modules. A module is a file containing JavaScript code and makes it easy to expose and hide certain values.
-// The module pattern is a design pattern that allows us to encapsulate private variables and methods. 
-// It is a way to create a private scope within a function.
