c22c4c5d84b078620d31e1927a9dd1f51b348f11
Paul Tobin <tobster111@gmail.com>
2023-03-05 09:52:49 +0000

    Javascript Patterns Singleton

---
+++ frontend-masters/javascript-patterns/design-patterns/singleton/exercise.js
@@ -0,0 +1,23 @@
+let instance;
+
+class DBConnection {
+  constructor(uri) {
+    if (instance) {
+      throw new Error('You can only create one instance!');
+    }
+    this.uri = uri;
+    instance = this;
+  }
+
+  connect() {
+    console.log(`DB ${this.uri} has been connected!`);
+  }
+
+  disconnect() {
+    console.log('DB disconnected');
+  }
+}
+
+const singletonConnection = Object.freeze(new DBConnection('mongodb://...'));
+
+const connection = new DBConnection('mongodb://...');
