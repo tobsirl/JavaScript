98ea6917b48960d064cc300254aba4d354bb1ecd
Paul Tobin <tobster111@gmail.com>
2023-03-05 09:48:37 +0000

    Javascript Patterns Singleton

--- frontend-masters/javascript-patterns/design-patterns/singleton/index.js
+++ frontend-masters/javascript-patterns/design-patterns/singleton/index.js
@@ -33,3 +33,28 @@ const singletonCounter = Object.freeze(new Counter());
 
 // 3. Exporting the variable as the `default` value within the file to make it globally accessible.
 export default singletonCounter;
+
+// Implementation Objects
+let counter = 0;
+
+// 1. Create an object containing the `getCount`, `increment`, and `decrement` method.
+const counterObject = {
+  getCount: () => counter,
+  increment: () => ++counter,
+  decrement: () => --counter,
+};
+
+// 2. Freeze the object using the `Object.freeze` method, to ensure the object is not modifiable.
+const singletonCounter = Object.freeze(counterObject);
+
+// 3. Export the object as the `default` value to make it globally accessible.
+export default singletonCounter;
+
+// We could even export the frozen object directly, without having to declare multiple variables.
+let counter = 0;
+
+export default Object.freeze({
+  getCount: () => counter,
+  increment: () => ++counter,
+  decrement: () => --counter,
+});
