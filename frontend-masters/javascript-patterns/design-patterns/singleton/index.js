7ec73d3981b495d60e3aa4e955567b1ea98079fd
Paul Tobin <tobster111@gmail.com>
2023-03-05 09:44:22 +0000

    Javascript Patterns Singleton

---
+++ frontend-masters/javascript-patterns/design-patterns/singleton/index.js
@@ -0,0 +1,28 @@
+// Singleton Pattern
+// Share a single global instance throughout our application
+
+// Implementation Classes
+let instance;
+// 1. Creating the `Counter` class, which contains a `constructor`, `getInstance`, `getCount`, `increment` and `decrement` method.
+// Within the constructor, we check to make sure the class hasn't already been instantiated.
+class Counter {
+  constructor() {
+    if (instance) {
+      throw new Error('You can only create one instance!');
+    }
+    this.counter = counter;
+    instance = this;
+  }
+
+  getCount() {
+    return this.counter;
+  }
+
+  increment() {
+    return ++this.counter;
+  }
+
+  decrement() {
+    return --this.counter;
+  }
+}
