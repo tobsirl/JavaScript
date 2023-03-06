e67f0017980ecbcca95fa4eeee7caa941383a065
Paul Tobin <tobster111@gmail.com>
2023-03-06 18:32:01 +0000

    Javascript Patterns Proxy

--- frontend-masters/javascript-patterns/design-patterns/proxy/index.js
+++ frontend-masters/javascript-patterns/design-patterns/proxy/index.js
@@ -7,3 +7,37 @@ const user = {
   age: 42,
   email: 'john@doe.com',
 };
+
+const userProxy = new Proxy(user, {
+  get: (obj, prop) => {
+    return `${new Date()} | The value of ${prop} is ${Reflect.get(obj, prop)}`;
+  },
+  set: (target, prop, value) => {
+    if (prop === 'email') {
+      if (!isValidEmail(value)) {
+        throw new Error('Invalid email');
+      }
+    }
+
+    if (prop === 'firstName' || prop === 'lastName') {
+      if (!isAllLetters(value)) {
+        throw new Error('Invalid name');
+      }
+    }
+
+    if (prop === 'age') {
+      if (typeof value !== 'number') {
+        throw new Error('Please provide a valid age.');
+      }
+
+      if (value < 18) {
+        throw 'User cannot be younger than 18.';
+      }
+    }
+
+    return Reflect.set(target, prop, value);
+  },
+});
+
+// userProxy.email = 'test@test.com';
+userProxy.get(user);
