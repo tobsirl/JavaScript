eef34d04efdef3360659e9d1a5a39630525b765f
Paul Tobin <tobster111@gmail.com>
2023-03-06 18:21:12 +0000

    Javascript Patterns Proxy

---
+++ frontend-masters/javascript-patterns/design-patterns/proxy/index.js
@@ -0,0 +1,9 @@
+import { isValidEmail, isAllLetters } from './validator.js';
+
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  username: 'johndoe',
+  age: 42,
+  email: 'john@doe.com',
+};
