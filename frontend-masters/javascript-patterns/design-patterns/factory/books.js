f396e1694470cbef053d6944d5f6ff5be0b7d7c8
Paul Tobin <tobster111@gmail.com>
2023-03-09 18:33:40 +0000

    Javascript Patterns Factory

---
+++ frontend-masters/javascript-patterns/design-patterns/factory/books.js
@@ -0,0 +1,29 @@
+export const book1 = {
+  title: 'Harry Potter',
+  author: 'JK Rowling',
+  isbn: 'AB123',
+};
+
+export const book2 = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  isbn: 'CD456',
+};
+
+export const book3 = {
+  title: 'Moby-Dick',
+  author: 'Herman Melville',
+  isbn: 'EF789',
+};
+
+export const book4 = {
+  title: 'Harry Potter',
+  author: 'JK Rowling',
+  isbn: 'AB123',
+};
+
+export const book5 = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  isbn: 'CD456',
+};
