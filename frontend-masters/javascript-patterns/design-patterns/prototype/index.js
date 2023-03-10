bf634ee25a00a6f1ff13b4500d1d5ec53a696423
Paul Tobin <tobster111@gmail.com>
2023-03-10 17:59:28 +0000

    Javascript Patterns Prototype

--- frontend-masters/javascript-patterns/design-patterns/prototype/index.js
+++ frontend-masters/javascript-patterns/design-patterns/prototype/index.js
@@ -1,29 +1,36 @@
-const createUser = ({ firstName, lastName, email }) => ({
-  firstName,
-  lastName,
-  fullName: `${firstName} ${lastName}`,
-  email,
-  checkLastOnline: () => {
-    console.log(`${this.fullName} was last online at ${Date.now()}`);
-  },
-  sendEmail: () => {
-    console.log(`Email sent to ${email}`);
-  },
-  delete: () => {
-    console.log('User removed');
-  },
-});
+class User {
+  constructor({ firstName, lastName, email }) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.fullName = `${firstName} ${lastName}`;
+    this.email = email;
+  }
 
-const user = createUser({
+  checkLastOnline() {
+    console.log(`${this.fullName} was last online at ${Date.now()}`);
+  }
+
+  sendEmail() {
+    console.log(`Email sent to ${this.email}`);
+  }
+
+  delete() {
+    console.log('User removed');
+  }
+}
+
+const user1 = new User({
   firstName: 'John',
   lastName: 'Doe',
   email: 'john@doe.com',
 });
 
-const user2 = createUser({
+const user2 = new User({
   firstName: 'Jane',
   lastName: 'Doe',
   email: 'jane@doe.com',
 });
 
-console.log(user.delete === user2.delete);
+console.log(user1, user2);
+
+// console.log(user.delete === user2.delete);
