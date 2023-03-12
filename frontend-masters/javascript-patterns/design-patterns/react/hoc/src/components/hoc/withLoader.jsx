bbcb7ed7bf91cad3a68eb28bb43bb2664a232b25
Paul Tobin <tobster111@gmail.com>
2023-03-12 09:52:30 +0000

    Javascript Patterns React HOC

--- frontend-masters/javascript-patterns/design-patterns/react/hoc/src/components/hoc/withLoader.jsx
+++ frontend-masters/javascript-patterns/design-patterns/react/hoc/src/components/hoc/withLoader.jsx
@@ -3,12 +3,16 @@ import { LoadingSpinner } from '../components/LoadingSpinner';
 
 export default function withLoader(Element, url) {
   return (props) => {
-    /* Add logic to:
-    1. Fetch data from the url that was passed as an argument.
-    2. Show the <LoadingSpinner /> while the  data is being fetched.
-    3. Pass the fetched data to the wrapped component.
-    */
+    const [data, setData] = React.useState(null);
 
-    return <Element {...props} />;
+    React.useEffect(() => {
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) => setData(res));
+    }, []);
+
+    if (!data) return <LoadingSpinner />;
+
+    return <Element {...props} data={data} />;
   };
 }
