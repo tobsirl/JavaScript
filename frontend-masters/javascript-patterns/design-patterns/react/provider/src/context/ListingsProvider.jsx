9f951f40aaddf975c7046de681489d78e3a08af8
Paul Tobin <tobster111@gmail.com>
2023-03-15 18:50:56 +0000

    Javascript Patterns React provider

--- frontend-masters/javascript-patterns/design-patterns/react/provider/src/context/ListingsProvider.jsx
+++ frontend-masters/javascript-patterns/design-patterns/react/provider/src/context/ListingsProvider.jsx
@@ -3,6 +3,18 @@ import useListings from '../hooks/useListings';
 
 export const ListingsContext = React.createContext(null);
 
+export function useListingsContext() {
+  const context = React.useContext(ListingsContext);
+
+  if (!context) {
+    throw new Error(
+      'useListingsContext must be used within a ListingsProvider'
+    );
+  }
+
+  return context;
+}
+
 export function ListingsProvider(props) {
   const listings = useListings();
 
