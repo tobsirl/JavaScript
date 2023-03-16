11143a24a7262af5e18c2994521d6ee83788ed3d
Paul Tobin <tobster111@gmail.com>
2023-03-16 17:37:50 +0000

    Javascript Patterns React compound

--- frontend-masters/javascript-patterns/design-patterns/react/compound/src/components/Input.jsx
+++ frontend-masters/javascript-patterns/design-patterns/react/compound/src/components/Input.jsx
@@ -1,41 +1,18 @@
 import React from 'react';
 import { useListingsContext } from '../context/ListingsProvider';
+import { FlyOut } from './FlyOut';
 
-export default function Input(props) {
+export default function Input() {
   const listings = useListingsContext();
 
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState('');
-  const toggle = React.useCallback(() => setOpen((state) => !state), []);
-
   return (
-    <div className="flyout">
-      <input
-        onFocus={toggle}
-        onBlur={toggle}
-        className="flyout-input"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        placeholder="Enter an address, city ,or ZIP code"
-        {...props}
-      />
-      {open && (
-        <div className="flyout-list">
-          <ul>
-            {listings.map((listing) => (
-              <li
-                key={listing.id}
-                onMouseDown={() => {
-                  setValue(listing.name);
-                }}
-                className="flyout-list-item"
-              >
-                {listing.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+    <FlyOut>
+      <FlyOut.Input />
+      <FlyOut.List>
+        {listings.map((listing) => (
+          <FlyOut.ListItem value={listing.name}>{listing.name}</FlyOut.ListItem>
+        ))}
+      </FlyOut.List>
+    </FlyOut>
   );
 }
