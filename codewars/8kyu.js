0be1a97bcdd5d9f22b2f1a08779d9caf505dac6b
Paul Tobin <tobster111@gmail.com>
2023-11-19 11:28:16 +0000

    Codewars

---
+++ codewars/8kyu.js
@@ -0,0 +1,25 @@
+// Fundamentals: Return 8kya
+// https://www.codewars.com/kata/55a5befdf16499bffb00007b/train/javascript
+function add(a, b) {
+  return a + b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function mod(a, b) {
+  return a % b;
+}
+
+function exponent(a, b) {
+  return a ** b;
+}
+
+function subt(a, b) {
+  return a - b;
+}
