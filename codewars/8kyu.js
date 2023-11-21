4d3a2c72ffe8faafe02b791fdb7b8bd0dfcfe3c3
Paul Tobin <tobster111@gmail.com>
2023-11-21 16:15:10 +0000

    Codewars

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -74,3 +74,11 @@ function answer3() {
   //the answer should be "yes" or "no"
   return "";
 }
+
+// Training JS #1: create your first JS function and print "Hello World!" (8kyu)
+// https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8/train/javascript
+function helloWorld() {
+  var str = "Hello World!";
+  console.log(str);
+  return str;
+}
