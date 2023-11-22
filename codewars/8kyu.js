8e45cfd8e9984a6855e9f628b8e4f9125b0b6b3b
Paul Tobin <tobster111@gmail.com>
2023-11-22 14:37:30 +0000

    Codewars

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -82,3 +82,10 @@ function helloWorld() {
   console.log(str);
   return str;
 }
+
+// Training JS #6: Basic data types--Boolean and conditional statements if..else (8kyu)
+// https://www.codewars.com/kata/571f832f07363d295d001ba8/train/javascript
+function trueOrFalse(val) {
+  if (val) return "true";
+  else return "false";
+}
