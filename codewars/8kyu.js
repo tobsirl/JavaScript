272f56f0f7ed910363cbacc01f1778472c1ec634
Paul Tobin <tobster111@gmail.com>
2023-11-24 15:07:42 +0000

    Codewars

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -129,3 +129,16 @@ function equal5() {
   var b = v3; //set number value to b
   return a % b;
 }
+
+// Fuel Calculator: Total Cost (8kyu)
+// https://www.codewars.com/kata/57b58827d2a31c57720012e8/train/javascript
+// In this kata you will have to write a function that takes litres and pricePerLiter as arguments. Purchases of 2 or more litres get a discount of 5 cents per litre, purchases of 4 or more litres get a discount of 10 cents per litre, and so on every two litres, up to a maximum discount of 25 cents per litre. But total discount per liter cannot be more than 25 cents. round answer to 2 decimal places. Also you can guess that there will not be negative or non-numeric inputs.
+function fuelPrice(litres, pricePerLiter) {
+  let discount = 0;
+  if (litres >= 2) discount = 0.05;
+  if (litres >= 4) discount = 0.1;
+  if (litres >= 6) discount = 0.15;
+  if (litres >= 8) discount = 0.2;
+  if (litres >= 10) discount = 0.25;
+  return Number((litres * (pricePerLiter - discount)).toFixed(2));
+}
