0eec96b27e4a8e8e86592467bef26d390e12ef0f
Paul Tobin <tobster111@gmail.com>
2023-11-25 09:25:49 +0000

    Codewars

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -142,3 +142,15 @@ function fuelPrice(litres, pricePerLiter) {
   if (litres >= 10) discount = 0.25;
   return Number((litres * (pricePerLiter - discount)).toFixed(2));
 }
+
+// Kata Example Twist (8kyu)
+// https://www.codewars.com/kata/525c1a07bb6dda6944000031/train/javascript
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+// Add the value "codewars" to the array websites/Websites 1,000 times.
+// var websites = []
+
+// Solution
+const websites = [];
+for (let i = 0; i < 1000; i++) {
+  websites.push("codewars");
+}
