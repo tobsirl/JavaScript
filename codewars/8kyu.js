03badd0c10459cabc70b7cc8d180234a741686be
Paul Tobin <tobster111@gmail.com>
2023-11-26 10:19:24 +0000

    Codewars

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -154,3 +154,37 @@ const websites = [];
 for (let i = 0; i < 1000; i++) {
   websites.push("codewars");
 }
+
+// Training JS #4: Basic data types--Array (8kyu)
+// https://www.codewars.com/kata/571effabb625ed9b0600107a/train/javascript
+// Task
+// I've written five function, each function receives a parameter: arr(an array), you should coding something with arr.
+//     1. getLength(arr)    should return length of arr
+//     2. getFirst(arr)     should return the first element of arr
+//     3. getLast(arr)      should return the last element of arr
+//     4. pushElement(arr)  should push an element to arr, and then return arr
+//     5. popElement(arr)   should pop an element from arr, and then return arr
+function getLength(arr) {
+  //return length of arr
+  return arr.length;
+}
+function getFirst(arr) {
+  //return the first element of arr
+  return arr[0];
+}
+function getLast(arr) {
+  //return the last element of arr
+  return arr[arr.length - 1];
+}
+function pushElement(arr) {
+  var el = 1;
+  //push el to arr
+  arr.push(el);
+
+  return arr;
+}
+function popElement(arr) {
+  //pop an element from arr
+  arr.pop();
+  return arr;
+}
