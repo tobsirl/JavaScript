55ea26bec91b28c1a8e118ce5bebd032ce16f4d0
Paul Tobin <tobster111@gmail.com>
2023-11-28 19:09:49 +0000

    Codewars

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -208,3 +208,27 @@ function remove(s, n) {
   }
   return arr.join("");
 }
+
+// Training JS #9: loop statement --while and do..while (8kyu)
+// https://www.codewars.com/kata/57216d4bcdd71175d6000560/train/javascript
+// Task
+// Coding in function padIt, function accept 2 parameters:
+// str, it's a string representing the string to pad, we need pad some "*" at leftside or rightside of str
+// n, it's a number, how many times to pad the string.
+// Behaviour
+// You need to write a loop statement within the function that loops n times. Each time through the loop it will add one * to str, alternating on which side it is padded: the first time will add a * to the left side of str, the second time will add a * to the right side, and so on.
+// Finally, return the padded string.
+// For example:
+// padIt("a",1) should return "*a"
+// padIt("a",2) should return "*a*"
+// padIt("a",3) should return "**a*"
+// padIt("a",4) should return "**a**"
+function padIt(str, n) {
+  let i = 0;
+  while (i < n) {
+    if (i % 2 === 0) str = "*" + str;
+    else str = str + "*";
+    i++;
+  }
+  return str;
+}
