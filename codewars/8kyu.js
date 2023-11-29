8ec20e9aad18317c5a4d614c928f93ff4da74335
Paul Tobin <tobster111@gmail.com>
2023-11-29 13:19:34 +0000

    Codewars

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -232,3 +232,17 @@ function padIt(str, n) {
   }
   return str;
 }
+
+// Color Ghost (8kyu)
+// https://www.codewars.com/kata/53f1015fa9fe02cbda00111a/train/javascript
+// Create a class Ghost
+// Ghost objects are instantiated without any arguments.
+// Ghost objects are given a random color attribute of white" or "yellow" or "purple" or "red" when instantiated
+// ghost = new Ghost();
+// ghost.color //=> "white" or "yellow" or "purple" or "red"
+class Ghost {
+  constructor() {
+    this.colors = ["white", "yellow", "purple", "red"];
+    this.color = this.colors[Math.floor(Math.random() * this.colors.length)];
+  }
+}
