4962c9f3ebbf506edceaf84958eb8fe05e51ae65
Paul Tobin <tobster111@gmail.com>
2023-12-02 10:43:01 +0000

    Codewars

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -261,3 +261,21 @@ function numberToPower(number, power) {
   }
   return result;
 }
+
+// Training JS #10: loop statement --for (8kyu)
+// https://www.codewars.com/kata/5721a78c283129e416000999/train/javascript
+// Task
+// Coding in function pickIt, function accept 1 parameter:arr, it's a number array, we need traverse arr by using for loop, if element is odd number, push it to array odd, if it's a even number, push it to array even.
+// I've defined two array odd and even in the function, and also wrote the return statement. your work is write a for loop.
+// If you forgot how to push an element to array, please refer to lesson 4.
+// If you forgot the usage of %, please refer to lesson 1.
+// Solution
+function pickIt(arr) {
+  var odd = [],
+    even = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) even.push(arr[i]);
+    else odd.push(arr[i]);
+  }
+  return [odd, even];
+}
