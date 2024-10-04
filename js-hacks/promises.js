4da53799e9251b6c0072a634c45081fceb7f8561
Paul Tobin <tobster111@gmail.com>
2024-10-04 21:36:30 +0100

    SW

--- js-hacks/promises.js
+++ js-hacks/promises.js
@@ -1,22 +1,35 @@
-function testPromises() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("Promise resolved");
-    }, 1000);
-  }).catch((error) => {
-    console.log("🚀 ~ error", error);
-    reject(error);
-  });
+// function testPromises() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Promise resolved");
+//     }, 1000);
+//   }).catch((error) => {
+//     console.log("🚀 ~ error", error);
+//     reject(error);
+//   });
+// }
+
+// const response = testPromises();
+// response
+//   .then((result, test) => {
+//     console.log("🚀 ~ result", result);
+//     test = "test";
+//     return [result, test];
+//   })
+//   .then((result) => {
+//     console.log("🚀 ~ result1", result[0]);
+//     console.log("🚀 ~ test", result[1]);
+//   });
+
+function fetchSWAPI() {
+  return fetch("https://swapi.dev/api/people/1")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("🚀 ~ data", data);
+      return data;
+    });
 }
 
-const response = testPromises();
-response
-  .then((result, test) => {
-    console.log("🚀 ~ result", result);
-    test = "test";
-    return [result, test];
-  })
-  .then((result) => {
-    console.log("🚀 ~ result1", result[0]);
-    console.log("🚀 ~ test", result[1]);
-  });
+fetchSWAPI().then(({ name, a, b, c, d, f }) => {
+  console.log({ name, a, b, c, d, f });
+});
