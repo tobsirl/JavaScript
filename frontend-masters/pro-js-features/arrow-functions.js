994303a264fcf609b4780e8b39282c882b88ab65
Paul Tobin <tobster111@gmail.com>
2024-10-06 10:05:31 +0100

    Pro JS

---
+++ frontend-masters/pro-js-features/arrow-functions.js
@@ -0,0 +1,32 @@
+// Traditional function expression
+var multiply = function (a, b) {
+  return a * b;
+};
+console.log(multiply(4, 3)); // Output: 12
+
+// Arrow function with implicit return
+const divide = (a, b) => a / b;
+console.log(divide(10, 2)); // Output: 5
+
+// Arrow function and lexical `this`
+function Counter() {
+  this.count = 0;
+
+  setInterval(() => {
+    this.count++;
+    console.log(this.count); // `this` refers to the Counter instance
+  }, 1000);
+}
+
+const counter = new Counter();
+
+// Arguments object in traditional function
+function showArguments() {
+  console.log(JSON.stringify(arguments));
+}
+showArguments(1, 2, 3); // Outputs: [1, 2, 3]
+
+// Arrow function and arguments object
+const showArrowArguments = () => {
+  console.log(JSON.stringify(arguments));
+};
