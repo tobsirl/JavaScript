aa8ff3ef7b1a2c91107e8fa8956832c39bc364f6
Paul Tobin <tobster111@gmail.com>
2024-10-06 10:00:03 +0100

    Pro JS

---
+++ frontend-masters/pro-js-features/template-strings.js
@@ -0,0 +1,17 @@
+// Use Template Strings to enhance the message declaration.
+
+let firstname = "Alice";
+let age = 25;
+let city = "New York";
+
+let message =
+  'Hello, my name is "' +
+  firstname +
+  '"' +
+  ". \nI am " +
+  age +
+  " years old and I live in " +
+  city +
+  ".";
+
+console.log(message);
