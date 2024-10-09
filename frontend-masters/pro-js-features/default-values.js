56925c5d9ca1cf1f168804c8649b38f0b3bd0c1c
Paul Tobin <tobster111@gmail.com>
2024-10-09 18:10:35 +0100

    Pro JS

---
+++ frontend-masters/pro-js-features/default-values.js
@@ -0,0 +1,11 @@
+// Use default values for arguments
+
+function calculateTax(price, tax) {
+  if (tax == undefined) {
+    tax = 0.2;
+  }
+  return price + price * tax;
+}
+
+console.log(calculateTax(1000, 0.1));
+console.log(calculateTax(1000));
