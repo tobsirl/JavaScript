04f622b7b18bc2c8ab17885e8e555b78b9be9c88
Paul Tobin <tobster111@gmail.com>
2024-10-15 18:25:17 +0100

    Pro JS

---
+++ frontend-masters/pro-js-features/string-methods.js
@@ -0,0 +1,11 @@
+/*
+Use startsWith(), endsWith(), and includes() to check substrings
+*/
+const str = "Hello, world!";
+console.log(str.startsWith("Hello"));
+console.log(str.endsWith("world!"));
+console.log(str.includes("o, w"));
+
+const original = "abc";
+const result = original.repeat(3);
+console.log(result);
