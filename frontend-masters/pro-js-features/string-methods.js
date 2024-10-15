075b37170d4d6de91a69d4abdc7380bfdbe86df1
Paul Tobin <tobster111@gmail.com>
2024-10-15 18:30:13 +0100

    Pro JS

--- frontend-masters/pro-js-features/string-methods.js
+++ frontend-masters/pro-js-features/string-methods.js
@@ -14,3 +14,11 @@ const strpadding = "5";
 const paddedStart = strpadding.padStart(5, "-");
 const paddedEnd = strpadding.padEnd(5, "-");
 console.log(paddedStart, paddedEnd);
+
+/*
+Use trimStart and trimEnd to trim whitespace from the string
+*/
+const strtrim = "   Hello, world!   ";
+const trimmedStart = strtrim.trimStart();
+const trimmedEnd = strtrim.trimEnd();
+console.log(trimmedStart, trimmedEnd);
