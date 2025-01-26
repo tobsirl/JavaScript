efce2b6b38ac8635e71c8843c38666657420dbe0
Paul Tobin <tobster111@gmail.com>
2025-01-26 11:36:32 +0000

    Refactor user group age logging to create an array of user groups from nested keys

--- objects/json.js
+++ objects/json.js
@@ -5,4 +5,16 @@ const segment = "age";
 
 // console.log(state.user_groups.age);
 
-console.log(Object.keys(state[userGroup][segment]));
+// console.log(Object.keys(state[userGroup][segment]));
+// console.log(Object.values(state[userGroup][segment]));
+// console.log(Object.entries(state[userGroup][segment]));
+
+const nestedUserGroups = Object.keys(state[userGroup][segment]);
+
+const userGroups = nestedUserGroups.map((nestedUserGroupsKey) => {
+  return {
+    [nestedUserGroupsKey]: state[userGroup][segment][nestedUserGroupsKey],
+  };
+});
+
+console.log(userGroups);
