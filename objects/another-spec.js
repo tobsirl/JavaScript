87cc38e688878562d4ef03f02da2cc7250da7654
Paul Tobin <tobster111@gmail.com>
2025-01-30 15:28:58 +0000

    Refactor user groups mapping to log device platforms by gender in another-spec.js

--- objects/another-spec.js
+++ objects/another-spec.js
@@ -1,8 +1,13 @@
 import state from "./another-spec.json" assert { type: "json" };
 
-const userGroups = state.user_groups;
-console.log("🚀 ~ userGroups:", userGroups);
+const userGroups = state.user_groups.gender;
 
-const mapOverUserGroups = Object.keys(userGroups).map((userGroup) => {
-  return userGroups[userGroup];
+Object.keys(userGroups).forEach((gender) => {
+  console.log(`Gender: ${gender}`);
+  const devicePlatforms = userGroups[gender].device_platform;
+  Object.keys(devicePlatforms).forEach((platform) => {
+    console.log(
+      `  Platform: ${platform}, Version: ${devicePlatforms[platform]}`
+    );
+  });
 });
